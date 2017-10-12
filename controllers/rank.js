4ac21e7021d1d98cec6e6d634b99e77c9b5edf83
Ricardo <ricardoplouis@gmail.com>
2017-10-12 11:02:48 -0500

    Documenting rankController and adding unit tests

--- controllers/rank.js
+++ controllers/rank.js
@@ -1,8 +1,19 @@
-export function getTilt(node){
-  return (node.rightCount - node.leftCount)
+/**
+* @method getTilt returns tilt value (-infinity, +infinity)
+* @param { Node = {rightCount: Number, leftCount: Number, ...}}
+* @returns {Number}
+*/
+
+export function getTilt(node = {leftCount: 0, rightCount: 0}){
+  return (node.rightCount && node.leftCount) ? node.rightCount - node.leftCount : 0;
 }
 
-function getSum(nodeList){
+/**
+* @method getSum get sum of the tilt values for all nodes in the array
+* @param {Array[Node]}
+* @return {Number}
+*/
+export function getSum(nodeList){
   let acc = 0;
   for (let node of nodeList){
     acc += getTilt(node);
@@ -10,20 +21,66 @@ function getSum(nodeList){
   return acc;
 }
 
-function getMean(nodeList){
+/**
+* @method getMean gets mean of the tilt values for all nodes in the array
+* @param {Array[Node]}
+* @return {Number}
+*/
+export function getMean(nodeList){
   let sum = getSum(nodeList);
   return (getSum(nodeList) / nodeList.length)
 }
 
-function getStdev(nodeList, mean){
+/**
+* @method getStdev gets standard deviation of the tilt values for all nodes in the array
+* @param {Array[Node]}
+* @return {Number}
+*/
+export function getStdev(nodeList, mean){
   var sumOfSquareDiff = 0;
   for (let node of nodeList){
     sumOfSquareDiff += Math.pow(getTilt(node) - mean, 2)
   }
   var variance = (sumOfSquareDiff / nodeList.length -1)
+  //NOTE: Returns to three fixed decimal points because of floating point arithmetic in JS
   var fixedVariance = Math.abs(variance.toFixed(3))
   var stdev = Math.pow(fixedVariance, 0.5);
   return stdev;
+}
+/**
+* @method makeQuartile creates upper and lower quartile limits
+* @param {Array[Node], number}
+* @return {Array[lowerQuartile, upperQuartile]}
+*/
+
+export function makeQuartile(nodeList, tilt){
+      let nodesInQuartile = [],
+          mean = getMean(nodeList),
+          stdev = getStdev(nodeList, mean),
+          upperLimit,
+          lowerLimit;
+      switch (tilt) {
+        case -2:
+          lowerLimit = -3 * stdev;
+          upperLimit = -2 * stdev;
+          break;
+        case -1:
+          lowerLimit = -2 * stdev;
+          upperLimit = -1 * stdev;
+          break;
+        case 0:
+          lowerLimit = -1 * stdev;
+          upperLimit = stdev;
+          break;
+        case 1:
+          lowerLimit = stdev;
+          upperLimit = stdev * 2;
+        case 2:
+          lowerLimit = stdev * 2;
+          upperLimit = stdev * 3
+          break;
+      }
+    return [lowerLimit, upperLimit];
 }
 
 export function getQuartile(nodeList, tilt){
--- tests/rankController.js
+++ tests/rankController.js
@@ -2,15 +2,35 @@ const assert = require('chai').assert;
 
 import { getTilt } from '../controllers/rank';
 
-var mockData = {
-  'leftCount': 10,
-  'rightCount': 5
-}
 
-describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -5 tilt of getTilt function', () => {
-      assert.equal(-5, getTilt(mockData));
+let mockData = [
+  {
+    'leftCount': 10,
+    'rightCount': 5
+  },
+  {
+    'leftCount': '5',
+    'rightCount': '2'
+  },
+  {
+    'leftCount': null,
+    'rightCount': 10
+  }
+]
+
+describe('RankController::', () => {
+  describe('getQuartile()', () => {
+    it('should return correct tilt value', () => {
+      assert.equal(-5, getTilt(mockData[0]));
+    });
+    it('should handle string inputs', () => {
+      assert.equal(-3, getTilt(mockData[1]));
+    });
+    it('should handle null values by returning 0', () => {
+      assert.equal(0, getTilt(mockData[2]));
+    });
+    it('should handle empty inputs by returning 0', () => {
+      assert.equal(0, getTilt({}));
     });
   });
 });
